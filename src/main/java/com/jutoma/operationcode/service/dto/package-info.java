ad20160dd9f326688290992c645d2bd51082dccc
Justin Rosenberg <justinksu@gmail.com>
2022-05-21 10:27:58 -0500

    Initial version of operationcode generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/jutoma/operationcode/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.jutoma.operationcode.service.dto;
---
+++ src/main/java/com/jutoma/operationcode/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.jutoma.operationcode.web.rest.vm;
